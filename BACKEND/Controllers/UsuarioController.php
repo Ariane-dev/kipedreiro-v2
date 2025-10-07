3c9c17c82e6d83357fc37f9ec816d5fddbe983ae
Ariane-dev <ariane.rosa500@gmail.com>
2025-10-07 11:12:53 -0300

    gyu

--- BACKEND/Controllers/UsuarioController.php
+++ BACKEND/Controllers/UsuarioController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Kipedreiro\Controllers;
 
+use App\Kipedreiro\Core\FileManager;
 use App\Kipedreiro\Models\Usuario;
 use App\Kipedreiro\Database\Database;
 use App\Kipedreiro\Core\View;
@@ -10,10 +11,33 @@ use App\Kipedreiro\Validadores\UsuarioValidador;
 class UsuarioController{
     public $usuario;
     public $db;
+    public $gerenciarImagem;
     public function __construct() {
         $this->db = Database::getInstance();
        $this->usuario = new Usuario($this->db);
+       $this->gerenciarImagem = new FileManager('upload');
     }
+    
+    public function salvarUsuario(){
+        $erros = UsuarioValidador::ValidarEntradas($_POST);
+        if(!empty($erros)){
+            Redirect::redirecionarComMensagem("usuario/criar", "error", implode("<br>", $erros));
+        }
+        $imagem = $this->gerenciarImagem->salvarArquivo($_FILES['imagem'], 'usuario');
+        if($this->usuario->inserirUsuario(
+            $_POST["nome_usuario"], 
+            $_POST["email_usuario"], 
+            $_POST["senha_usuario"], 
+            $_POST["tipo_usuario"],
+             "Ativo",
+             $imagem
+             )){
+            Redirect::redirecionarComMensagem("usuario/listar", "success", "Usuário cadastrado com sucesso!");
+        }else{
+            Redirect::redirecionarComMensagem("usuario/criar", "error", "Erro ao cadastrar usuário!");
+        }
+    }
+
     // index
     public function index(){
         $resultado = $this->usuario->buscarUsuarios();
@@ -28,24 +52,22 @@ class UsuarioController{
     public function viewCriarUsuarios(){
         View::render("usuario/create");
     }
-    public function viewEditarUsuarios(){
-        View::render("usuario/edit");
+    public function viewEditarUsuarios(int $id){
+        $dados = $this->usuario->buscarUsuariosPorID($id);
+        foreach($dados as $usuario){
+            $dados = $usuario;
+
+        }
+        View::render("usuario/edit", ["usuario"=>$dados]);
     }
-    public function viewExcluirUsuarios(){
-        View::render("usuario/delete");
+    public function viewExcluirUsuarios($id){
+        View::render("usuario/delete", ["id_usuario"=>$id]);
+    }
+    public function relatorioUsuario($id, $dataInicial, $dataFinal){
+        View::render("usuario/relatorio", 
+        ["id"=>$id, "dataInicial"=>$dataInicial, "dataFinal"=>$dataFinal]);
     }
 
-    public function salvarUsuario(){
-        $erros = UsuarioValidador::ValidarEntradas($_POST);
-        if(!empty($erros)){
-            Redirect::redirecionarComMensagem("usuario/criar", "error", implode("<br>", $erros));
-        }
-        if($this->usuario->inserirUsuario($_POST["nome_usuario"], $_POST["email_usuario"], $_POST["senha_usuario"], $_POST["tipo_usuario"], "Ativo")){
-            Redirect::redirecionarComMensagem("usuario/listar", "success", "Usuário cadastrado com sucesso!");
-        }else{
-            Redirect::redirecionarComMensagem("usuario/criar", "error", "Erro ao cadastrar usuário!");
-        }
-    }
  
     public function atualizarUsuario(){
         echo "atualizar Usuario";
